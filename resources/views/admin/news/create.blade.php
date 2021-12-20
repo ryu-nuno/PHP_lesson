ff3d5cde5421aa71fa7810909baec73a9f795f93
ryu-nuno <shion9bsk@gmail.com>
2021-12-20 00:06:37 +0000

    error mysql socket

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -3,7 +3,7 @@
 
 
 {{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
-@section('title', 'ニュースの新規作成')
+@section('title', 'ニュース新規作成')
 
 {{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
 @section('content')
